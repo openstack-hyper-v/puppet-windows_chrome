c918e70ade78e21bc13c1defa6784356937ce7e5
Peter Pouliot <ppouliot@gmail.com>
2013-10-17 22:44:06 -0400

    adding remove

---
+++ manifests/disable.pp
@@ -0,0 +1,21 @@
+# === Class: windows_chrome::disable
+#
+# This class removes the installed components.
+
+class windows_chrome::disable (
+  $package   = $::windows_chrome::params::package,
+) inherits windows_chrome::params {
+
+  package { $package:
+    ensure  => absent,
+  }
+
+  windows_path { $chrome_path:
+    ensure  => absent,
+  }
+
+  file { "${::temp}/${package}.exe":
+    ensure  => absent,
+  }
+
+}
